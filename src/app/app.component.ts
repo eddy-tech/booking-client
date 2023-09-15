222ae62ab575ce81454326db5b338a990793350e
kouekamngambou <kouekamngambou@et.intechinfo.fr>
2023-09-15 05:58:07 +0200

    Ajout de la fonctionnalité X

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,20 @@
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'booking-client';
+}
+
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
